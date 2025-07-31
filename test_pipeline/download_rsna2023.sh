ef4c69fefacccc97a54368a2ccfca8dc2808b0e9
tianyu han tianyu.han@pennmedicine.upenn.edu <hanti@cubic-login1.bicic.local>
2025-07-31 12:40:14 -0400

    fix

--- test_pipeline/download_rsna2023.sh
+++ test_pipeline/download_rsna2023.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=21-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --mem=160G
 #SBATCH --partition=long
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tianyu.han@pennmedicine.upenn.edu
